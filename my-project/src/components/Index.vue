3317e734aeeb217291a3f9442edacc595722e6d8
maoyining <2044964754@qq.com>
2020-07-04 18:41:59 +0800

    lunbo

---
+++ my-project/src/components/Index.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <neighbor-switch
+    :data="imageList"
+    v-model="currentIndex"
+  >
+    <template v-slot:content="scoped">
+      <img :src="scoped.attribute.url"/>
+    </template>
+  </neighbor-switch>
+</div>
+</template>
+
+<script>
+import NeighborSwitch from './NeighborSwitch'
+export default {
+  name: 'Index',
+  components: {
+    NeighborSwitch: NeighborSwitch,
+  },
+  data() {
+    return {
+      currentIndex:0,
+      imageList: [{
+        url:'./p1.jpg'
+      },{
+        url:'./p2.jpg'
+      },{
+        url:'./p3.jpg'
+      },{
+        url:'./p4.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-project/src/components/NeighborSwitch.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-button">
+      <button class="button-left" @click="prev">&lt;</button>
+    </div>
+    <slot name="content" :attribute="imageList[currentIndex]"></slot>
+    <div class="wrapper-button">
+      <button class="button-right" @click="next">&gt;</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'NeighborSwitch',
+  props:{
+    data: Array,
+    value: Number
+  },
+  data(){
+    return {
+      currentIndex: this.value,
+      imageList: this.data
+    }
+  },
+  methods:{
+    next(){
+      if(this.currentIndex<this.imageList.length-1){
+        this.currentIndex++;
+      }else{
+        this.currentIndex = 0;
+      }
+    },
+    prev(){
+      if(this.currentIndex!==0){
+        this.currentIndex--;
+      }else{
+        this.currentIndex = this.imageList.length-1
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper{
+  width:800px;
+  height:400px;
+  line-height:400px;
+  border:solid 1px #eee;
+  background-size: contain;
+  position: relative;
+}
+.wrapper img{
+  width:600px;
+  height:400px;
+  vertical-align: top;
+}
+.wrapper-button{
+  line-height:400px;
+  display:inline-block;
+  width:100px;
+  height:100%;
+  vertical-align: top;
+}
+.button-right,.button-left {
+  display:inline-block;
+  margin:0 auto;
+  border-radius: 50%;
+  width:50px;
+  height:50px;
+  border:0;
+  font-size:25px;
+  color:#ccc;
+  outline: none;
+  cursor: pointer;
+}
+
+</style>
